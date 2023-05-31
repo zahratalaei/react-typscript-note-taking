f67aaacc9a8528d8356e98c2473181fb8bac0247
zahratalaei <zahratalaei@users.noreply.github.com>
2023-05-31 10:58:55 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Container } from 'react-bootstrap'
+import {Routes,Route,Navigate} from 'react-router-dom'
+import NewNote from './components/NewNote'
+export type Note = {
+  id:string,
+}& NoteData
+export type NoteData = {
+  title: string,
+  markdown:string,
+  tags: Tag[]
+}
+export type Tag = {
+  id: string,
+  label:string
+}
+function App() {
+
+  return (
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/new' element={<NewNote/>} />
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to="/"/>} />
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
